a206ee4b64f27dc253d74e7be79fd6329b91a495
klupsky <katharina@chalupsky.eu>
2024-05-10 13:09:55 +0200

    hero

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,14 @@ import logo from './logo.svg';
 
 export default function Home() {
   return (
-    <main className="font-sans flex min-h-screen flex-col items-center bg-red">
-      <div className="">
-        COME AS YOU ARE
-        <Image src={logo} className="App-logo" alt="logo" />
+    <main className="font-sans">
+      <div className="relative flex min-h-screen flex-col gap-y-5 items-center text-blue bg-red justify-center">
+        <Image src={logo} alt="Leandra Logo" />
+        <div class="uppercase flex flex-col text-xl text-center tracking-[.4em]">
+          <h1>X</h1>
+          <h1>Café</h1>
+          <h1>Leopold</h1>
+        </div>
       </div>
     </main>
   );
