2e890a7d7541df318e91c356bf6169fcfc231489
klupsky <katharina@chalupsky.eu>
2024-05-10 18:11:15 +0200

    backup

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,24 @@
+'use client';
+
 import Footer from '../components/Footer';
 import Hero from '../components/Hero';
 import InfoSection from '../components/InfoSection';
+import MotionBounceIn from '../components/MotionBounceIn';
 
 export default function Home() {
   return (
     <main className="font-lexend relative ">
-      <Hero />
-      <InfoSection />
-      <Footer />
+      <MotionBounceIn>
+        <Hero />
+      </MotionBounceIn>
+
+      <MotionBounceIn>
+        <InfoSection />
+      </MotionBounceIn>
+
+      <MotionBounceIn>
+        <Footer />
+      </MotionBounceIn>
     </main>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,20 @@
 import Image from 'next/image';
 import logo from '../../public/logo.svg';
 import pattern from '../../public/pattern.svg';
+import MotionBounce from './MotionBounce';
 
 export default function Hero() {
   return (
     <div class="h-screen w-full relative">
       <div className="absolute inset-0 z-0 bg-red ">
-        <Image
-          src={pattern}
-          alt="Leandra Pattern"
-          layout="fill"
-          objectFit="cover"
-        />
+        <MotionBounce>
+          <Image
+            src={pattern}
+            alt="Leandra Pattern"
+            layout="fill"
+            objectFit="cover"
+          />
+        </MotionBounce>
       </div>
       <div className="relative p-5 lg:p-10 flex min-h-screen flex-col gap-y-5 items-center text-blue justify-center">
         <Image src={logo} alt="Leandra Logo" />
